abfb449a23247a3984fd48b37ed420b005ae08c2
aronvaupel <vaupelaron@gmail.com>
2021-08-16 14:02:46 +0200

    Fix typo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vite/plugin-react-refresh';
 
 export default defineConfig({
+  plugins: [reactRefresh()],
   build: {
     outDir: 'dist/app',
   },
