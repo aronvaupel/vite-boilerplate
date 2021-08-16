5e0c07ab9d93d8164b26acd9cc65a7d1e50e205b
aronvaupel <vaupelaron@gmail.com>
2021-08-16 12:22:25 +0200

    Setup server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('hello server!');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}!`);
+});
